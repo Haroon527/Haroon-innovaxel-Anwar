a0e1b16ad232a6a24ffaead04f003913dd85c063
Haroon Anwar <142000850+Haroon527@users.noreply.github.com>
2025-03-23 16:27:07 +0500

    Functions to interact with the database

--- app.py
+++ app.py
@@ -10,3 +10,8 @@ urls_collection = db["urls"]
 def generate_short_code(length=6):
     characters = string.ascii_letters + string.digits
     return ''.join(random.choices(characters, k=length))
+def find_url_by_short_code(short_code):
+    return urls_collection.find_one({"shortCode": short_code})
+
+def find_url_by_original_url(original_url):
+    return urls_collection.find_one({"url": original_url})
